fdc48cb2abeeeffcc6ef9057b6b0a200a94bab86
mdziaro <mdziaro@student.agh.edu.pl>
2023-10-08 23:50:02 +0200

    added run()

--- lab01/skrypt1.py
+++ lab01/skrypt1.py
@@ -1,5 +1,6 @@
 import sys
 
+descriptions = {'f': "Zwierzak idzie do przodu", 'b':"Zwierzak idzie do tyłu", 'l':"Zwierzak skręca w lewo", 'r':"Zwierzak skręca w prawo"}
 
 def display(args,show_index):
     for i in range(len(args)):
@@ -8,6 +9,13 @@ def display(args,show_index):
         print(args[i])
     return 0
 
+
+def run(moves, move_descriptions):
+    for i in moves:
+        if i in move_descriptions:
+            print(move_descriptions[i])
+               
+
 if __name__ == "__main__":
     show_index=sys.argv[1]
     args=sys.argv[2:]
@@ -15,6 +23,12 @@ if __name__ == "__main__":
         print("Set first argument as \"True\" or \"False\"")
         exit(1)
     print("System wystartował.")
+    print("Funkcja pierwsza:\nStart")
     display(args, show_index)
+    print("Stop")
+    if show_index == "False":
+        print("Funkcja druga:\nStart")
+        run(args, descriptions)
+        print("Stop")
     print("System zakończył działanie.")
     exit(0)
