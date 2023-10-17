cde07721d0063655325e10fd1ab28ef6adbb1245
mdziaro <mdziaro@student.agh.edu.pl>
2023-10-17 18:47:05 +0200

    made it so -i doesn't change output case

--- lab02/grep.py
+++ lab02/grep.py
@@ -5,17 +5,20 @@ def grep(pattern, input_text, ignore_case=False, whole_word=False):
     result = []
 
     for line in lines:
-        if ignore_case:
-            line = line.lower()
+        line_to_compare = line if not ignore_case else line.lower()
 
         words = line.split()
 
         for word in words:
+            word_to_compare = word if not ignore_case else word.lower()
+
             if whole_word:
-                if word == pattern:
+                if word_to_compare == pattern:
                     result.append(line)
                     break
             else:
-                if pattern in word:
+                if pattern in word_to_compare:
                     result.append(line)
                     break
+
+    return '\n'.join(result)
