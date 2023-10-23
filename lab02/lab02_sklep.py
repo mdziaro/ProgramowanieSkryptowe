06a7893ebbee66e0d2052c6b8bb4bf5a1c8ec4b1
mdziaro <mdziaro@student.agh.edu.pl>
2023-10-23 17:25:12 -0400

    bugfixes, final(?) implementation

--- lab02/lab02_sklep.py
+++ lab02/lab02_sklep.py
@@ -1,113 +1,103 @@
 import sys
+import re
 
+class Shop:
+    def __init__(self, filename):
+        self.filename = filename
+        self.products = {}
+        self.customers = {}
 
-def load_data(file_name):
-    try:
-        with open(file_name, 'r') as file:
-            data = file.read()
-        return data
-    except FileNotFoundError:
-        print(f"Plik {file_name} nie istnieje.")
-        sys.exit(1)
-
-
-def display_warehouse(data):
-    lines = data.strip().split('\n')
-    print("--------------+------------ ")
-    print(lines[0])  # Nazwa | Ilosc
-    print("--------------+------------ ")
-    for line in lines[1:]:
-        print(line)
-
-
-def display_customer_purchases(data, customers):
-    for customer in customers:
-        print(customer)
-        lines = data.get(customer, '')
-        if not lines:
-            continue
-        lines = lines.strip().split('\n')
-        print("--------------+------------ ")
-        print(lines[0])  # Klient
-        print("--------------+------------ ")
-        print(lines[1])  # Nazwa | Ilosc
-        for line in lines[2:]:
-            print(line)
-        print("\n")
-
-
-
-def process_sell_command(data, sell_command):
-    sell_parts = sell_command.split()
-    if len(sell_parts) % 2 != 0:
-        print("Niepoprawny format komendy")
-        return
-
-    for i in range(0, len(sell_parts), 2):
-        customer = sell_parts[i]
-        purchases = sell_parts[i + 1]
-        purchases = purchases.split(':')
-        customer_data = data.get(customer, '')
-        customer_lines = customer_data.strip().split('\n')
-        for purchase in purchases:
-            product, quantity = purchase.split('(')
-            product = product.strip()
-            quantity = int(quantity[:-1].strip())
-            for j in range(2, len(customer_lines)):
-                parts = customer_lines[j].split()
-                if parts[0] == product:
-                    available_quantity = int(parts[1])
-                    if available_quantity >= quantity:
-                        customer_lines[j] = f"{product:<13}{available_quantity - quantity}"
-                    else:
-                        print(f"{product} - nie ma takiego towaru")
-            data[customer] = "\n".join(customer_lines)            
-
-
-def main(file_name):
-    data = load_data(file_name)
-    lines = data.strip().split('\n')
-    warehouse_data = {}
-    customer_data = {}
-    current_customer = None
-
-    for line in lines[2:]:
-        if not line:
-            current_customer = None
-        elif current_customer:
-            customer_data[current_customer] += '\n' + line
-        else:
-            parts = line.split()
-            if len(parts) == 2:
-                product, quantity = parts
-                warehouse_data[product] = int(quantity)
-            elif len(parts) == 1:
-                current_customer = parts[0]
-                customer_data[current_customer] = ''
-
-    while True:
+    def load_data(self):
         try:
-            command = input("> ")
-            if not command:
+            with open(self.filename, 'r') as file:
+                lines = file.readlines()
+                for line in lines:
+                    line = line.strip()
+                    if line.startswith("Product:"):
+                        product_info = line.split(":")[1].strip().split(",")
+                        product_name, product_quantity = product_info[0], int(product_info[1])
+                        self.products[product_name] = product_quantity
+                    elif line.startswith("Customer:"):
+                        customer_name = line.split(":")[1].strip()
+                        self.customers[customer_name] = {}
+
+        except FileNotFoundError:
+            print("File not found. Make sure the file exists.")
+            sys.exit(1)
+
+    def warehouse(self):
+        print("Warehouse:")
+        print("-------------+------------")
+        print("Nazwa towaru | Ilość sztuk")
+        print("-------------+------------")
+        for product, quantity in self.products.items():
+            print(f"{product.ljust(13)}{quantity}")
+        print()
+
+    def sell(self, command):
+        customers_data = command.split()
+        for i in range(1, len(customers_data)):
+            customer_and_products = customers_data[i].split(":")
+            customer_name = customer_and_products[0]
+            products = customer_and_products[1].split(",")
+            customer_dict = self.customers.get(customer_name)
+            if customer_dict is None:
+                print(f"Customer '{customer_name}' not found.")
                 continue
-        except EOFError:  # Obsługa Ctrl+D (Linux, macOS) / Ctrl+Z Enter (Windows)
-            break
 
-        command_parts = command.split()
-        if command_parts[0] == "warehouse":
-            display_warehouse(data)
-        elif command_parts[0] == "show":
-            customers_to_show = command_parts[1:]
-            display_customer_purchases(customer_data, customers_to_show)
-        elif command_parts[0] == "sell":
-            process_sell_command(customer_data, command)
-        else:
-            print("Nieznana komenda")
+            for product_info in products:
+                product_name, quantity = product_info.split("(")
+                product_name = product_name.strip()
+                quantity = int(quantity[:-1])
+                if product_name not in self.products:
+                    print(f"Product '{product_name}' does not exist in the warehouse.")
+                elif self.products[product_name] < quantity:
+                    print(f"Not enough '{product_name}' in the warehouse for customer '{customer_name}'.")
+                elif quantity < 0:
+                    print(f"Cannot sell negative quantity of '{product_name}' to customer '{customer_name}'.")
+                else:
+                    customer_dict[product_name] = customer_dict.get(product_name, 0) + quantity
+                    self.products[product_name] -= quantity
 
-if __name__ == "__main__":
+    def show(self, command):
+        customers_data = command.split()
+        for i in range(1, len(customers_data)):
+            customer_name = customers_data[i]
+            customer_dict = self.customers.get(customer_name)
+            if customer_dict is None:
+                print(f"Customer '{customer_name}' not found.")
+            else:
+                print(f"{customer_name}:")
+                print("-------------+------------")
+                print("Nazwa towaru | Ilość sztuk")
+                print("-------------+------------")
+                for product, quantity in customer_dict.items():
+                    print(f"{product.ljust(13)}{quantity}")
+                print()
+
+    def process_commands(self):
+        while True:
+            try:
+                command = input("> ")
+                if not command:
+                    continue
+                elif command == "warehouse":
+                    self.warehouse()
+                elif command.startswith("sell"):
+                    self.sell(command)
+                elif command.startswith("show"):
+                    self.show(command)
+                else:
+                    print("Unknown command.")
+            except EOFError:
+                break
+
+if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print("usage: sklep.py <file_name>")
+        print("Usage: sklep.py [filename]")
         sys.exit(1)
 
-    file_name = sys.argv[1]
-    main(file_name)
+    filename = sys.argv[1]
+    shop = Shop(filename)
+    shop.load_data()
+    shop.process_commands()
