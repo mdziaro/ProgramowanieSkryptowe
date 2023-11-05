e94985d984081fed4bbb1e92f4d8aaf408b76e3d
mdziaro <mdziaro@student.agh.edu.pl>
2023-11-05 08:22:02 -0500

    test compatibility, initial commit

--- lab04/main.py
+++ lab04/main.py
@@ -1,6 +1,28 @@
-from model import MapDirection, MoveDirection, Vector2d
+import sys
+from model import MoveDirection, Vector2d
+from controller import Simulation, OptionsParser
 
-print(MapDirection.EAST)                
-print(MapDirection.EAST.next())         
-print(MapDirection.EAST.previous())     
-print(MapDirection.EAST.toUnitVector())
+directions: list[MoveDirection] = OptionsParser.parse(sys.argv[1:]) 
+positions: list[Vector2d] = [Vector2d(2,2), Vector2d(3,4)] # Pozycje początkowe dla zwierząt, odpowiednio, (2,2) oraz (3,4) 
+simulation: Simulation = Simulation(directions, positions)
+simulation.run()     
+
+#python3 main.py f b r l f f r r f f f f f f f f
+"""
+Zwierzę 0 : (2,3) ↑
+Zwierzę 1 : (3,3) ↑
+Zwierzę 0 : (2,3) →
+Zwierzę 1 : (3,3) ←
+Zwierzę 0 : (3,3) →
+Zwierzę 1 : (2,3) ←
+Zwierzę 0 : (3,3) ↓
+Zwierzę 1 : (2,3) ↑
+Zwierzę 0 : (3,2) ↓
+Zwierzę 1 : (2,4) ↑
+Zwierzę 0 : (3,1) ↓
+Zwierzę 1 : (2,4) ↑
+Zwierzę 0 : (3,0) ↓
+Zwierzę 1 : (2,4) ↑
+Zwierzę 0 : (3,0) ↓
+Zwierzę 1 : (2,4) ↑
+"""
